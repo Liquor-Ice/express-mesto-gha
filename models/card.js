e6fda50a4cb26d7fd625219270b9f7591df8576a
Liquor-Ice <doppelgangerer@yandex.ru>
2024-01-25 10:03:56 +0300

    fix like schema2

--- models/card.js
+++ models/card.js
@@ -16,11 +16,10 @@ const cardSchema = new mongoose.Schema({
     ref: 'user',
     required: true,
   },
-  likes: {
+  likes: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'user',
-    default: [],
-  },
+  }],
   createAt: {
     type: Date,
     default: Date.now,
