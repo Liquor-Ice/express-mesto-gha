831443eb7e3c90606764504409f8e3eba9fd6a83
Liquor-Ice <doppelgangerer@yandex.ru>
2024-01-30 02:52:05 +0300

    test deleteCard again

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,6 +1,6 @@
 const Card = require('../models/card');
 const NotFoundError = require('../errors/NotFoundError');
-const ForbiddenError = require('../errors/ForbiddenError');
+// const ForbiddenError = require('../errors/ForbiddenError');
 
 module.exports.getCards = (req, res, next) => {
   Card.find({})
@@ -23,15 +23,16 @@ module.exports.deleteCard = (req, res, next) => {
   )
     // eslint-disable-next-line consistent-return
     .then((card) => {
-      const { owner } = card.owner;
-      const user = req.user._id;
-      if (owner !== user) {
-        throw new ForbiddenError('Требуется авторизация');
-      }
-      Card.findByIdAndDelete(cardId).orFail(
-        () => new NotFoundError('Данная карточка не найдена'),
-      )
-        .then((delCard) => res.status(200).send({ data: delCard }));
+      const owner = card.owner.toString();
+      const user = req.user._id.toString();
+      // if (owner !== user) {
+      //   throw new ForbiddenError('Требуется авторизация');
+      // }
+      // Card.findByIdAndDelete(cardId).orFail(
+      //   () => new NotFoundError('Данная карточка не найдена'),
+      // )
+      //   .then((delCard) => res.status(200).send({ data: delCard }));
+      return res.status(200).send({ compare: owner !== user });
     })
     .catch(next);
 };
