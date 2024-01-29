ebdeeec12da691ad66ff1facfce9bc00a013051c
Liquor-Ice <doppelgangerer@yandex.ru>
2024-01-30 02:02:05 +0300

    test deleteCards3

--- controllers/cards.js
+++ controllers/cards.js
@@ -12,7 +12,7 @@ module.exports.createCard = (req, res, next) => {
   const { name, link } = req.body;
 
   Card.create({ name, link, owner: req.user._id })
-    .then((card) => res.status(201).send({ data: card }))
+    .then((card) => res.status(201).send({ data: card, owner: card.owner, user: req.user._id }))
     .catch(next);
 };
 
